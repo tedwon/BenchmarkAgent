4c12b6f20d65deb973e9e426cecb7c977c312931
tedwon <iamtedwon@gmail.com>
2015-06-27 15:40:41 +0900

    Add BenchmarkAgent

---
+++ src/main/java/com/realtimecep/BenchmarkAgent.java
@@ -0,0 +1,12 @@
+package com.realtimecep;
+
+/**
+ * <p/>
+ *
+ * @author <a href=mailto:iamtedwon@gmail.com">Ted Won</a>
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+public class BenchmarkAgent {
+
+}
